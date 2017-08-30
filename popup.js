74a5ebfe31eb7618248f051a76e38465e84b5499
xn066417 <wujunxi@xiaoniu66.com>
2017-08-30 18:16:47 +0800

    update

--- popup.js
+++ popup.js
@@ -4,4 +4,12 @@ $(function() {
         console.log(res);
     });
 
+    // 4c8e00eef3453ca6b511eed85fdeb71951756823
+    $.get('https://api.github.com/?access_token=4c8e00eef3453ca6b511eed85fdeb71951756823', function(res) {
+        console.log(res);
+    })
+
+    chrome.bookmarks.getTree(function(args) {
+        console.log(args);
+    });
 });
